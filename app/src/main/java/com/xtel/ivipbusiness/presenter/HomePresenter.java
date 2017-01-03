cb1b65ad1681871755f6b7de9c4988d94799c69c
leconglongvu <leconglongvu@gmail.com>
2017-01-03 10:33:25 +0700

    Change package

---
+++ app/src/main/java/com/xtel/ivipbusiness/presenter/HomePresenter.java
@@ -0,0 +1,16 @@
+package com.xtel.ivipbusiness.presenter;
+
+import com.xtel.ivipbusiness.view.activity.inf.IHomeView;
+
+/**
+ * Created by Lê Công Long Vũ on 12/29/2016
+ */
+
+public class HomePresenter {
+
+    private IHomeView iHome;
+
+    public HomePresenter(IHomeView iHome) {
+        this.iHome = iHome;
+    }
+}
---
+++ app/src/main/java/com/xtel/ivipbusiness/view/activity/inf/IHomeView.java
@@ -0,0 +1,12 @@
+package com.xtel.ivipbusiness.view.activity.inf;
+
+import android.app.Activity;
+
+/**
+ * Created by Lê Công Long Vũ on 12/27/2016
+ */
+
+public interface IHomeView {
+
+    Activity getActivity();
+}
