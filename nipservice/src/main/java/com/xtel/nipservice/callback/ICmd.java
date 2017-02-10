8a4e7acabe2e45e717f33e112cbeafd2ea9bff90
leconglongvu <leconglongvu@gmail.com>
2017-02-10 22:58:09 +0700

    Clear Project

--- nipservice/src/main/java/com/xtel/nipservice/callback/ICmd.java
+++
@@ -1,9 +0,0 @@
-package com.xtel.nipservice.callback;
-
-/**
- * Created by vihahb on 1/5/2017.
- */
-
-public interface ICmd {
-    void execute();
-}
--- nipservice/src/main/java/com/xtel/nipservice/model/entity/RESP_None.java
+++
@@ -1,8 +0,0 @@
-package com.xtel.nipservice.model.entity;
-
-/**
- * Created by Lê Công Long Vũ on 1/6/2017
- */
-
-public class RESP_None extends RESP_Basic {
-}
