3fba3ee46afdd0891eb1fd4da1806da1947e9783
leconglongvu <leconglongvu@gmail.com>
2017-04-20 09:54:17 +0700

    Update Dialog Manager

--- app/src/main/java/com/xtel/sdk/utils/DialogManager.java
+++ app/src/main/java/com/xtel/sdk/utils/DialogManager.java
@@ -67,168 +67,177 @@ public class DialogManager {
         dialog.show();
     }
 
+    @SuppressWarnings("ConstantConditions")
     public void chooseColor(final Activity activity, View view, final CallbackIntListener callbackIntListener) {
         try {
             //We need to get the instance of the LayoutInflater, use the context of this activity
-            LayoutInflater inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            //Inflate the view from a predefined XML layout
-            View layout = inflater.inflate(R.layout.popup_color, (ViewGroup) activity.findViewById(R.id.popup_color_viewgroup));
-            // create a 300px width and 470px height PopupWindow
-            final PopupWindow popupWindow = new PopupWindow(activity);
-            popupWindow.setContentView(layout);
-            popupWindow.setOutsideTouchable(true);
-            popupWindow.setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+//            LayoutInflater inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//            //Inflate the view from a predefined XML layout
+//            View layout = inflater.inflate(R.layout.popup_color, (ViewGroup) activity.findViewById(R.id.popup_color_viewgroup));
+//            // create a 300px width and 470px height PopupWindow
+//            final PopupWindow popupWindow = new PopupWindow(activity);
+//            popupWindow.setContentView(layout);
+//            popupWindow.setOutsideTouchable(true);
+//            popupWindow.setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
 
-            layout.findViewById(R.id.popup_color_one).setOnClickListener(new View.OnClickListener() {
+            final Dialog dialog = new Dialog(activity, R.style.Theme_Transparent);
+            dialog.setContentView(R.layout.popup_color);
+            dialog.getWindow().setLayout(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            dialog.getWindow().setGravity(Gravity.CENTER);
+            dialog.setCancelable(true);
+            dialog.setCanceledOnTouchOutside(true);
+
+            dialog.findViewById(R.id.popup_color_one).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    popupWindow.dismiss();
+                    dialog.dismiss();
                     callbackIntListener.negativeClicked(R.color.color_1);
                 }
             });
-            layout.findViewById(R.id.popup_color_two).setOnClickListener(new View.OnClickListener() {
+            dialog.findViewById(R.id.popup_color_two).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    popupWindow.dismiss();
+                    dialog.dismiss();
                     callbackIntListener.negativeClicked(R.color.color_2);
                 }
             });
-            layout.findViewById(R.id.popup_color_three).setOnClickListener(new View.OnClickListener() {
+            dialog.findViewById(R.id.popup_color_three).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    popupWindow.dismiss();
+                    dialog.dismiss();
                     callbackIntListener.negativeClicked(R.color.color_3);
                 }
             });
-            layout.findViewById(R.id.popup_color_four).setOnClickListener(new View.OnClickListener() {
+            dialog.findViewById(R.id.popup_color_four).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    popupWindow.dismiss();
+                    dialog.dismiss();
                     callbackIntListener.negativeClicked(R.color.color_4);
                 }
             });
-            layout.findViewById(R.id.popup_color_five).setOnClickListener(new View.OnClickListener() {
+            dialog.findViewById(R.id.popup_color_five).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    popupWindow.dismiss();
+                    dialog.dismiss();
                     callbackIntListener.negativeClicked(R.color.color_5);
                 }
             });
-            layout.findViewById(R.id.popup_color_six).setOnClickListener(new View.OnClickListener() {
+            dialog.findViewById(R.id.popup_color_six).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    popupWindow.dismiss();
+                    dialog.dismiss();
                     callbackIntListener.negativeClicked(R.color.color_6);
                 }
             });
-            layout.findViewById(R.id.popup_color_seven).setOnClickListener(new View.OnClickListener() {
+            dialog.findViewById(R.id.popup_color_seven).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    popupWindow.dismiss();
+                    dialog.dismiss();
                     callbackIntListener.negativeClicked(R.color.color_7);
                 }
             });
-            layout.findViewById(R.id.popup_color_eight).setOnClickListener(new View.OnClickListener() {
+            dialog.findViewById(R.id.popup_color_eight).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    popupWindow.dismiss();
+                    dialog.dismiss();
                     callbackIntListener.negativeClicked(R.color.color_8);
                 }
             });
-            layout.findViewById(R.id.popup_color_nine).setOnClickListener(new View.OnClickListener() {
+            dialog.findViewById(R.id.popup_color_nine).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    popupWindow.dismiss();
+                    dialog.dismiss();
                     callbackIntListener.negativeClicked(R.color.color_9);
                 }
             });
-            layout.findViewById(R.id.popup_color_ten).setOnClickListener(new View.OnClickListener() {
+            dialog.findViewById(R.id.popup_color_ten).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    popupWindow.dismiss();
+                    dialog.dismiss();
                     callbackIntListener.negativeClicked(R.color.color_10);
                 }
             });
-            layout.findViewById(R.id.popup_color_eleven).setOnClickListener(new View.OnClickListener() {
+            dialog.findViewById(R.id.popup_color_eleven).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    popupWindow.dismiss();
+                    dialog.dismiss();
                     callbackIntListener.negativeClicked(R.color.color_11);
                 }
             });
-            layout.findViewById(R.id.popup_color_twelve).setOnClickListener(new View.OnClickListener() {
+            dialog.findViewById(R.id.popup_color_twelve).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    popupWindow.dismiss();
+                    dialog.dismiss();
                     callbackIntListener.negativeClicked(R.color.color_12);
                 }
             });
-            layout.findViewById(R.id.popup_color_thirteen).setOnClickListener(new View.OnClickListener() {
+            dialog.findViewById(R.id.popup_color_thirteen).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    popupWindow.dismiss();
+                    dialog.dismiss();
                     callbackIntListener.negativeClicked(R.color.color_13);
                 }
             });
-            layout.findViewById(R.id.popup_color_forteen).setOnClickListener(new View.OnClickListener() {
+            dialog.findViewById(R.id.popup_color_forteen).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    popupWindow.dismiss();
+                    dialog.dismiss();
                     callbackIntListener.negativeClicked(R.color.color_14);
                 }
             });
-            layout.findViewById(R.id.popup_color_fifteen).setOnClickListener(new View.OnClickListener() {
+            dialog.findViewById(R.id.popup_color_fifteen).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    popupWindow.dismiss();
+                    dialog.dismiss();
                     callbackIntListener.negativeClicked(R.color.color_15);
                 }
             });
-            layout.findViewById(R.id.popup_color_sixteen).setOnClickListener(new View.OnClickListener() {
+            dialog.findViewById(R.id.popup_color_sixteen).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    popupWindow.dismiss();
+                    dialog.dismiss();
                     callbackIntListener.negativeClicked(R.color.color_16);
                 }
             });
-            layout.findViewById(R.id.popup_color_seventeen).setOnClickListener(new View.OnClickListener() {
+            dialog.findViewById(R.id.popup_color_seventeen).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    popupWindow.dismiss();
+                    dialog.dismiss();
                     callbackIntListener.negativeClicked(R.color.color_17);
                 }
             });
-            layout.findViewById(R.id.popup_color_eighteen).setOnClickListener(new View.OnClickListener() {
+            dialog.findViewById(R.id.popup_color_eighteen).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    popupWindow.dismiss();
+                    dialog.dismiss();
                     callbackIntListener.negativeClicked(R.color.color_18);
                 }
             });
-            layout.findViewById(R.id.popup_color_nineteen).setOnClickListener(new View.OnClickListener() {
+            dialog.findViewById(R.id.popup_color_nineteen).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    popupWindow.dismiss();
+                    dialog.dismiss();
                     callbackIntListener.negativeClicked(R.color.color_19);
                 }
             });
-            layout.findViewById(R.id.popup_color_twenty).setOnClickListener(new View.OnClickListener() {
+            dialog.findViewById(R.id.popup_color_twenty).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    popupWindow.dismiss();
+                    dialog.dismiss();
                     callbackIntListener.negativeClicked(R.color.color_21);
                 }
             });
-            layout.findViewById(R.id.popup_color_btn_positive).setOnClickListener(new View.OnClickListener() {
+            dialog.findViewById(R.id.popup_color_btn_positive).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     callbackIntListener.positiveClicked();
-                    popupWindow.dismiss();
+                    dialog.dismiss();
                 }
             });
 
             // display the popup in the center
-            popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+//            popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+            dialog.show();
         } catch (Exception e) {
             e.printStackTrace();
         }
